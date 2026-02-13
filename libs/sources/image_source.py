025acceb39e93d9347bc7bb790ee7adc60498155
jeongsehyeon <3ayhyun@gmail.com>
2026-02-14 00:26:17 +0900

    [implement] image_sorce.py

--- libs/sources/image_source.py
+++ libs/sources/image_source.py
@@ -1,12 +1,13 @@
 import os
 import glob
 import cv2
+import time
 
 class ImageSource:
     def __init__(self, config):
         self.config = config
-        self.assets_path = config.get('assets_path', 'asstes/images')
-        self.pattern = config.get('fattern', '*jpg')
+        self.assets_path = config.get('assets_path', 'assets/images')
+        self.pattern = config.get('pattern', '*.jpg')
 
         self.files = glob.glob(os.path.join(self.assets_path, self.pattern)) # glob.glob를 통해 pattern 형태의 파일명을 리스트로 반환한다 
         # 파일을 숫자순으로 정렬
@@ -15,6 +16,8 @@ class ImageSource:
         self.idx = 0
 
         self.fps = config.get('fps', 30)
+        self.interval = 1.0 / self.fps # 1초에 fps장의 이미지를 보여주기 위한 이미지 한장을 보여주는 시간
+        self.last_time = time.perf_counter()
 
 
     def read(self):
@@ -25,9 +28,18 @@ class ImageSource:
 
             return frame_id, image
         '''
-        image = cv2.imread(self.files[self.idx]) # cv2를 이용했으므로 BGR 형태
-        frame_id = self.idx
+        # 1/fps 만큼의 시간이 지나지 않았다면 남은 시간만큼 sleep을 건다 
+        now_time = time.perf_counter()
+        time_gap = now_time - self.last_time
+        if time_gap < self.interval:
+            time.sleep(self.interval - time_gap)
+        self.last_time = time.perf_counter()
 
-        self.idx += 1 # 다음번 read 호출시 다음프레임 이미지를 반환하기 위해 
+        if self.idx < len(self.files):
+            image = cv2.imread(self.files[self.idx]) # cv2를 이용했으므로 BGR 형태
+            frame_id = self.idx
+            self.idx += 1 # 다음 read 호출시 다음프레임 이미지를 반환하기 위해 
+        else: # 더이상 읽어들일 파일이 없는 경우
+            frame_id, imagea = None, None
 
         return frame_id, image
